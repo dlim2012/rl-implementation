706c863b2064d9a5ce637fe9b2501844f141301c
Jung Hoon Lim <57515449+dlim2012@users.noreply.github.com>
2022-12-06 18:30:47 -0500

    Update Baseline_Mountain_Car.py
    Update hyperparameters

--- Baseline_Mountain_Car.py
+++ Baseline_Mountain_Car.py
@@ -7,26 +7,27 @@ from tools.tools import time_hr_min_sec
 
 if __name__ == '__main__':
 
-    w_hidden_sizes = [50] * 2
-    theta_hidden_sizes = [50] * 3
-    w_lr = 1e-7
-    theta_lr = 3e-9
-    input_mode = 'default'
-    n = 10 ** 5
-
-    MDP = get_Mountain_Car_MDP(n_bins=100)
-    agent = MLP_Agent(
-        MDP=MDP,
-        state_size=2,
-        w_hidden_sizes=w_hidden_sizes,
-        w_lr=w_lr,
-        theta_hidden_sizes=theta_hidden_sizes,
-        theta_lr=theta_lr,
-        input_mode=input_mode
-    )
-    MDP.calc_optimal_v_pi(agent)
-
     for i in range(1000):
+        w_hidden_sizes = [50] * 2
+        theta_hidden_sizes = [50] * 3
+        w_lr = 1e-8
+        theta_lr = 1e-8
+        input_mode = 'default'
+        n = 10 ** 5
+        print(w_hidden_sizes, theta_hidden_sizes, w_lr, theta_lr)
+
+        MDP = get_Mountain_Car_MDP(n_bins=100)
+        agent = MLP_Agent(
+            MDP=MDP,
+            state_size=2,
+            w_hidden_sizes=w_hidden_sizes,
+            w_lr=w_lr,
+            theta_hidden_sizes=theta_hidden_sizes,
+            theta_lr=theta_lr,
+            input_mode=input_mode
+        )
+        MDP.calc_optimal_v_pi(agent)
+
         start = time.time()
         returns = Baseline_Algorithm(
             MDP,
